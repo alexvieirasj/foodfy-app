8f8af17411b7fafcf66fe464a78fc9fc4433b260
Alex Vieira <alexvieiracb20@gmail.com>
2020-07-27 23:22:25 -0300

    Ajustes do nunjcks nas páginas

--- public/script.js
+++ public/script.js
@@ -1,15 +1,27 @@
+
 const modalOverlay = document.querySelector('.modal-overlay');
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card_index');
+const cards_recipes = document.querySelectorAll('.card_recipe');
+
+for(let card of cards_recipes) {
+    card.addEventListener('click', function(){
+        const recipeId = card.getAttribute('id');
+        window.location.href = `/receitas/${recipeId}`; //envia para a página receitas com id selecionado
+    });
+}
+       
 
 for(let card of cards) {
     card.addEventListener('click', function(){
-        const videoId = card.getAttribute('id');
+        const videoId = card.getAttribute('id'); //pega o id
+        const src = card.querySelector('img').src; //pega a img
+
         const nameValue = card.getElementsByTagName('h3')[0].innerHTML;
         const authorValue = card.getElementsByTagName('p')[0].innerHTML;
 
         console.log(nameValue)
         modalOverlay.classList.add('active');
-        modalOverlay.querySelector('img').src = `assets/${videoId}.png`; //busca na propriedade diretamente
+        modalOverlay.querySelector('img').src = src; //busca na propriedade diretamente
         modalOverlay.querySelector('h3').innerHTML = `${nameValue}`;
         modalOverlay.querySelector('p').innerHTML = `${authorValue}`;
     });
--- server.js
+++ server.js
@@ -18,6 +18,23 @@ server.get("/", function(req, res){
     return res.render("index", { items: recipes });
 });
 
+server.get("/sobre", function(req, res){
+    return res.render("sobre");
+});
+
+server.get("/receitas", function(req, res){
+    return res.render("receitas", { items: recipes });
+});
+
+server.get("/receitas/:index", function (req, res) {
+  
+    const recipeIndex = req.params.index;
+
+    console.log(recipes[recipeIndex]);
+    return res.render("receita", { item: recipes[recipeIndex] });
+ 
+});
+
 server.listen(5000, function() {
     console.log("server is running");
 });
